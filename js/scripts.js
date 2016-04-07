24c8b87ffa671498ec203bc40f98a9d89695c0d2
Sue Jefthas <sueadriejefthas@gmail.com>
2016-04-07 21:34:32 +0200

    Added files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,29 @@ $(document).ready(function(){
 		
     });
 });
+
+//SMOOTH SCROLLING
+$(document).ready(function() {
+	$(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+		event.preventDefault;
+		var hash = this.hash;
+		
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 900, function(){
+			window.location.hash = hash;
+		});
+	});
+});
+
+//SLIDE IN ANIMATION
+$(window).scroll(function() {
+	$(".slideanim").each(function(){
+		var pos = $(this).offset().top;
+		
+		var winTop = $(window).scrollTop();
+		if(pos < winTop + 600) {
+			$(this).addClass("slide");
+		}
+	});
+});
